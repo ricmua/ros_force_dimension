6506e80d95bcaf9d677a84ddded6563d0b2ac79b
ricmua <ricmua@whit.contact>
2022-12-05 13:43:14 -0500

    Corrected a small error in the CMake file that prevented a Windows build (untested since change, but verified prior).

--- ForceDimensionSDKConfig.cmake
+++ ForceDimensionSDKConfig.cmake
@@ -8,7 +8,7 @@ elseif(WIN32 OR MSVC)
   set(ForceDimensionSDK_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include")
   set(FD_DHDMS_LIB "${CMAKE_CURRENT_LIST_DIR}/lib/dhdms.lib")
   set(FD_DHDMS64_LIB "${CMAKE_CURRENT_LIST_DIR}/lib/dhdms64.lib")
-  set(ForceDimensionSDK_LIBRARIES "${FD_DHDMS_LIB} ${FD_DHDMS64_LIB}")
+  set(ForceDimensionSDK_LIBRARIES "${FD_DHDMS_LIB};${FD_DHDMS64_LIB}")
 endif()
 
 
